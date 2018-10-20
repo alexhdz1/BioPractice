cf73da742368737e1da3bfaef4be3498e597cdf4
carlos-bl <carlosb@ciencias.unam.mx>
2018-10-19 22:46:03 -0500

    Avance en crear nuevo material; solo falta resolver un problema con la base

--- src/main/java/com/muyalware/biopractice/web/NuevoMaterialBean.java
+++ src/main/java/com/muyalware/biopractice/web/NuevoMaterialBean.java
@@ -5,90 +5,55 @@
  */
 package com.muyalware.biopractice.web;
 
-import java.io.Serializable;
+import com.muyalware.biopractice.controller.MaterialJpaController;
+import com.muyalware.biopractice.controller.IllegalOrphanException;
+import com.muyalware.biopractice.model.Material;
+import com.muyalware.biopractice.model.PersistenceUtil;
+import java.util.List;
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.SessionScoped;
+import javax.faces.bean.RequestScoped;
+
 /**
  *
- * @author hp
+ * @author alexis
  */
-@ManagedBean(name="NuevoMaterialBean")
-@SessionScoped
-public class NuevoMaterialBean implements Serializable{
-    
-    private static final long serialVersionUID = 1715935052239888761L;
-    private String nombre;
-    private String descripcion;
-    private int unidades;
-    private boolean prestado;
-    private String categoria;
-    private String subcategoria;
-    private String color;
-        
+@ManagedBean(name ="NuevoMaterialBean")
+@RequestScoped
+public class NuevoMaterialBean {
 
+    private final MaterialJpaController jpa;
+    private Material material;
+    
+
+    /**
+     * Creates a new instance of MaterialController
+     */
     public NuevoMaterialBean() {
+        jpa = new MaterialJpaController(PersistenceUtil.getEntityManagerFactory());
+        material = new Material();
+}
+
+    
+    public Material getMaterial() {
+        return material;
+    }  
+
+    public void setMaterial(Material m) {
+        material = m;
     }
 
-    public NuevoMaterialBean(String nom, String des) {
-	this.nombre = nom;
-	this.descripcion = des;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public String getDescripcion() {
-        return descripcion;
-    }
-
-    public void setDescripcion(String descripcion) {
-        this.descripcion = descripcion;
-    }
-
-    public int getUnidades() {
-        return unidades;
-    }
-
-    public void setUnidades(int unidades) {
-        this.unidades = unidades;
-    }
-
-    public boolean isPrestado() {
-        return prestado;
-    }
-
-    public void setPrestado(boolean prestado) {
-        this.prestado = prestado;
-    }
-
-    public String getCategoria() {
-        return categoria;
-    }
-
-    public void setCategoria(String categoria) {
-        this.categoria = categoria;
-    }
-
-    public String getSubcategoria() {
-        return subcategoria;
-    }
-
-    public void setSubcategoria(String subcategoria) {
-        this.subcategoria = subcategoria;
-    }
-
-    public String getColor() {
-        return color;
-    }
-
-    public void setColor(String color) {
-        this.color = color;
+    public String addMaterial() throws IllegalOrphanException, com.muyalware.biopractice.controller.exceptions.exceptions.IllegalOrphanException {
+        jpa.create(material);
+        return "lista";
     }
     
+    public String agregaMaterial() throws IllegalOrphanException, com.muyalware.biopractice.controller.exceptions.exceptions.IllegalOrphanException {
+        jpa.create(material);
+        return "lista";
+    }
+
+    public List<Material> getRegistrados() {
+        return jpa.findMaterialEntities();
+    }
 
 }
