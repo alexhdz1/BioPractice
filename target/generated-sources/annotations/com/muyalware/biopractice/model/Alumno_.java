a327b25ac8b828eeb068457461933e244e0f46a6
kyusol <kyus.soto@gmail.com>
2018-10-21 13:49:38 -0500

    login

--- target/generated-sources/annotations/com/muyalware/biopractice/model/Alumno_.java
+++ target/generated-sources/annotations/com/muyalware/biopractice/model/Alumno_.java
@@ -5,7 +5,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-10-18T19:17:19")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-10-19T10:25:42")
 @StaticMetamodel(Alumno.class)
 public class Alumno_ { 
 
