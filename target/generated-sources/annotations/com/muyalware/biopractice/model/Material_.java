5ef9daf7a47a3e0e21c86540d7af66e9d4551806
kyusol <kyus.soto@gmail.com>
2018-11-17 18:15:22 -0600

    Modificacion Alumnos.xhtml

--- target/generated-sources/annotations/com/muyalware/biopractice/model/Material_.java
+++ target/generated-sources/annotations/com/muyalware/biopractice/model/Material_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-11-17T15:59:48")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-11-17T17:41:27")
 @StaticMetamodel(Material.class)
 public class Material_ { 
 
