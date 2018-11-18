3895357137364ecf733945f7da0eca8c58cb5736
kyusol <kyus.soto@gmail.com>
2018-11-17 18:47:43 -0600

    Correción Alumnos.xhtml

--- target/generated-sources/annotations/com/muyalware/biopractice/model/Administrador_.java
+++ target/generated-sources/annotations/com/muyalware/biopractice/model/Administrador_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-11-17T18:39:30")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-11-17T18:46:06")
 @StaticMetamodel(Administrador.class)
 public class Administrador_ { 
 
