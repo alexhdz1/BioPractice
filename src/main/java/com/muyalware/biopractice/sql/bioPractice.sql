cd72633d828fcefdf4a9739349ed3dc62cfedb63
kyusol <kyus.soto@gmail.com>
2018-12-04 16:41:13 -0600

    Modificación Script SQL

--- src/main/java/com/muyalware/biopractice/sql/bioPractice.sql
+++ src/main/java/com/muyalware/biopractice/sql/bioPractice.sql
@@ -53,3 +53,28 @@ CREATE TABLE material(
 insert into Administrador (id, nombre, correo, contrasena, activo) values (1, 'Jule Clemson', 'directional@ciencias.unam.mx', 'fT1tIW', false);
 insert into Administrador (id, nombre, correo, contrasena, activo) values (2, 'Norah Bew', 'intranet@ciencias.unam.mx', 'EIsNFw', false);
 insert into Administrador (id, nombre, correo, contrasena, activo) values (3, 'Mellie Dwane', 'Mandatory@ciencias.unam.mx', 's0keKR', false);
+
+create or replace function bytea_import(p_path text, p_result out bytea) 
+                   language plpgsql as $$
+declare
+  l_oid oid;
+  r record;
+begin
+  p_result := '';
+  select lo_import(p_path) into l_oid;
+  for r in ( select data 
+             from pg_largeobject 
+             where loid = l_oid 
+             order by pageno ) loop
+    p_result = p_result || r.data;
+  end loop;
+  perform lo_unlink(l_oid);
+end;$$;
+
+INSERT INTO alumno(nombre, correo, contrasena, num_cuenta, estado, fotografia, activo) VALUES('Alexis', 'alexhdz1@ciencias.unam.mx', 'alxhdz1', '315123450', 'FALSE', bytea_import('/Users/kyuSolesito/Downloads/Image/m.jpg'), 'FALSE');
+INSERT INTO alumno(nombre, correo, contrasena, num_cuenta, estado, fotografia, activo) VALUES('Carlos', 'carlitos@ciencias.unam.mx', 'carlos', '56788767', 'FALSE', bytea_import('/Users/kyuSolesito/Downloads/Image/n.jpg'), 'FALSE');
+INSERT INTO alumno(nombre, correo, contrasena, num_cuenta, estado, fotografia, activo) VALUES('Gibran', 'gazmatem@ciencias.unam.mx', 'zorra', '95445678', 'FALSE', bytea_import('/Users/kyuSolesito/Downloads/Image/o.jpg'), 'FALSE');
+INSERT INTO alumno(nombre, correo, contrasena, num_cuenta, estado, fotografia, activo) VALUES('Martin', 'martin1@ciencias.unam.mx', 'martin', '2345676565', 'FALSE', bytea_import('/Users/kyuSolesito/Downloads/Image/p.jpg'), 'FALSE');
+INSERT INTO alumno(nombre, correo, contrasena, num_cuenta, estado, fotografia, activo) VALUES('Gerardo', 'g.soto@ciencias.unam.mx', 'yo', '2346456356', 'FALSE', bytea_import('/Users/kyuSolesito/Downloads/Image/q.jpg'), 'FALSE');
+INSERT INTO alumno(nombre, correo, contrasena, num_cuenta, estado, fotografia, activo) VALUES('Jaime', 'henry@ciencias.unam.mx', 'pelos', '245672454', 'FALSE', bytea_import('/Users/kyuSolesito/Downloads/Image/r.jpg'), 'FALSE');
+INSERT INTO alumno(nombre, correo, contrasena, num_cuenta, estado, fotografia, activo) VALUES('Hanna', 'profa@ciencias.unam.mx', 'prof', '236345634', 'FALSE', bytea_import('/Users/kyuSolesito/Downloads/Image/s.jpg'), 'FALSE');
